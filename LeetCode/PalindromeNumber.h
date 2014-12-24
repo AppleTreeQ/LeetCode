6a6b8210e23ff009857d7bebc3b1b841747bb0d2
Qian Zhai <zhaiqianzq@gmail.com>
2014-12-24 21:27:02 +0900

    chang file name for LongestPalindromicSubstring.h

---
+++ LeetCode/PalindromeNumber.h
@@ -0,0 +1,13 @@
+//
+//  PalindromeNumber.h
+//  LeetCode
+//
+//  Created by ipuser on 2014/12/24.
+//  Copyright (c) 2014年 ipuser. All rights reserved.
+//
+
+#ifndef LeetCode_PalindromeNumber_h
+#define LeetCode_PalindromeNumber_h
+
+
+#endif

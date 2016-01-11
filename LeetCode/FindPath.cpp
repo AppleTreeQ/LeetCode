a4872ddc624daff97502b4d4f11aad5a1641a556
QianZhai <qzhai@oku-dhcp18.lrlab>
2016-01-11 16:56:15 +0900

    add FindPath.h

---
+++ LeetCode/FindPath.cpp
@@ -0,0 +1,9 @@
+//
+//  FindPath.cpp
+//  LeetCode
+//
+//  Created by QianZhai on 6/18/15.
+//  Copyright (c) 2015 ipuser. All rights reserved.
+//
+
+#include "FindPath.h"
---
+++ LeetCode/FindPath.h
@@ -0,0 +1,15 @@
+//
+//  FindPath.h
+//  LeetCode
+//
+//  Created by QianZhai on 6/18/15.
+//  Copyright (c) 2015 ipuser. All rights reserved.
+//
+
+#ifndef __LeetCode__FindPath__
+#define __LeetCode__FindPath__
+
+#include <stdio.h>
+
+
+#endif /* defined(__LeetCode__FindPath__) */
